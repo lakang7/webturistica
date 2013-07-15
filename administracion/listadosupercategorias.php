49f9b3b82159daa18b4a52d4baf0d0f220fa7cbb
Vanessa Suárez <vane.sb@gmail.com>
2013-07-14 21:35:26 -0430

    Validación ANTES de eliminar un registro de categoría
    Se creó una función JS para confirmar si realmente se quiere eliminar un
    registro ANTES de eliminarlo y asi evitar eliminar por error

--- administracion/listadosupercategorias.php
+++ administracion/listadosupercategorias.php
@@ -9,22 +9,28 @@
 	
     <link rel="stylesheet" href="../css/administracion/estructura.css" type="text/css"  />
 	<link rel="stylesheet" type="text/css" href="../css/administracion/component.css" />
-	<link rel="stylesheet" type="text/css" href="../css/administracion/default.css" />
-    
-    <link href='http://fonts.googleapis.com/css?family=Oswald:400,700' rel='stylesheet' type='text/css' />
-    
+	<link rel="stylesheet" type="text/css" href="../css/administracion/default.css" />    
+    <link href='http://fonts.googleapis.com/css?family=Oswald:400,700' rel='stylesheet' type='text/css' />    
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
     <script src="../js/administracion/modernizr.custom.js"></script>
-    <script src="../js/administracion/jquery.dlmenu.js"></script>
-    
-
+    <script src="../js/administracion/jquery.dlmenu.js"></script>	
+	<script type="text/javascript" language="javascript">
+		//Funcion para preguntar si esta seguro de eliminar un registro ANTES de proceder a eliminarlo realmente
+		function confirmar(url){ 
+			if (!confirm("¿Está seguro de que desea eliminar el registro? Presione ACEPTAR para borrar o CANCELAR para volver al listado")) { 
+				return false; 
+		    } 
+			else { 
+				document.location=url; 
+				return true; 
+			} 
+		} 
+	</script>
 </head>
 
 <body onload="cargo()">
 	<div class="banner"></div>
-    <div class="menu">    				
-		<?php menu_administrativo();  ?>		                       
-    </div>
+    <div class="menu"><?php menu_administrativo();?></div>
     <div class="panel">
     	<div class="titulo_panel">Listado Super Categoría</div>
         <div class="opcion_panel">
@@ -38,21 +44,28 @@
                     	<td>Código</td><td>Descripción Súper Categoría</td><td width="20"></td><td  width="20"></td>
                     </tr>
                 </thead>
-
                 <tbody>
-                    <?php
-					    $con = conectarse();
-			 		    $sql_select="select * from supercategoria order by idsupercategoria";
-					    $result_select = pg_exec($con,$sql_select);
-					    for($i=0;$i<pg_num_rows($result_select);$i++){
-					       $supercategoria=pg_fetch_array($result_select,$i);					   
-						   ?>
-						   <tr><td><?php echo  Codigo("SUP",$supercategoria[0]); ?></td><td><?php echo $supercategoria[1]; ?></td><td title="Editar" style="cursor:pointer;"><a href="editarsupercategorias.php?id=<?php echo $supercategoria[0]; ?>" ><img src="../imagenes/edit.png" width="16" height="16" /></a></td><td title="Eliminar" style="cursor:pointer;"><a href="eliminar.php?clave=1&id=<?php echo $supercategoria[0]; ?>"><img src="../imagenes/delete.png" width="16" height="16" /></a></td></tr>					    
-						   <?php
-                        }
-					
-					?>
-					               
+                <?php
+				$con = conectarse();
+			 	$sql_select = "select * from supercategoria order by idsupercategoria";
+				$result_select = pg_exec($con,$sql_select);
+				for($i=0;$i<pg_num_rows($result_select);$i++){
+				    $supercategoria = pg_fetch_array($result_select,$i);	
+					$idSuperCategoria = $supercategoria[0];
+				    ?>
+					<tr>
+						<td>
+						<?php echo Codigo("SUP",$supercategoria[0]); ?>
+						</td>
+						<td><?php echo $supercategoria[1]; ?>
+						</td>
+						<td title="Editar" style="cursor:pointer;"><a href="editarsupercategorias.php?id=<?php echo $supercategoria[0]; ?>" ><img src="../imagenes/edit.png" width="16" height="16" /></a></td>
+						<td title="Eliminar" style="cursor:pointer;"><a href="javascript:;" onClick="confirmar('eliminar.php?clave=1&id=<?php echo $idSuperCategoria;?>'); return false;"><img src="../imagenes/delete.png" width="16" height="16" /></a>
+						</td>
+					</tr>					    
+					<?php
+                }				
+				?>					               
                 </tbody>
             </table>
         </div>
